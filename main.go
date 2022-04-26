43954498a0d43b41586dfcd8047b9cfce86220d3
Cosmin Smechea <ext-cosmin.smechea@goflink.com>
2022-04-26 18:05:38 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func main() {
+	engine := NewResizeEngine()
+
+	r := mux.NewRouter()
+	// I've been using closures here for the following reasons:
+	// 1. to flex
+	// 2. to not create a controller
+	// I would use a controller as a jumping point to the actual business logic and from there to persistence
+	// also to make sure the payload/url params/variables are correct and populated accordingly
+
+	r.HandleFunc("/v1/resize",
+		func(w http.ResponseWriter, r *http.Request) { // using closures here just to flex
+			// { "urls": ["a","b","c"] }
+			// BIG ASSUMPTION here that the payload is *correct*
+			// in a production environment we would validate the payload via the playground validator
+			// also we'd make sure all fields are populated
+			// normally we would have a json friendly error type with relevant info i.e. what part of the payload failed
+			// or if the error was internal or of a 3rd party i.e. imgur is down
+			// these are usually detailed in a spec of the architect's choice i.e. swagger or openapi
+			var async bool
+			val, ok := r.URL.Query()["async"]
+			if !ok {
+				async = false
+			} else {
+				async, _ = strconv.ParseBool(val[0])
+			}
+
+			type PL struct {
+				URLs []string `json:"urls"`
+			}
+			var payload PL
+			// the part below is very slimmed down
+			// ideally we would enforce a maxbytesreader on the request body and disallow unknown fields
+			body, _ := io.ReadAll(r.Body)
+			dec := json.NewDecoder(bytes.NewReader(body))
+
+			_ = dec.Decode(&payload)
+
+			// return err for duplicates OR remove duplicates directly, whatever works
+			uniqueURLs := removeDuplicateStr(payload.URLs)
+			// switch cases for async here and point to different looper functions
+			var res []byte
+			switch async {
+			case true:
+				res, _ = json.Marshal(engine.HashImageAsync(uniqueURLs))
+			case false:
+				// the trick here is we want to concurrently process images even when
+				// async is false but wait for all the results before displaying them
+				// this is preferred from a performance point of view as opposed to iterating through the url slice
+				// create an output channel per request and receive the set of processed images on that channel only
+				res, _ = json.Marshal(engine.HashImageSync(uniqueURLs))
+			}
+
+			_, _ = w.Write(res)
+		},
+	).Methods(http.MethodPost)
+	r.HandleFunc("/v1/image/{image_hash}.jpeg",
+		func(w http.ResponseWriter, r *http.Request) {
+			// again, horribly slimmed down
+			// these should be checked
+			_, cancel := context.WithTimeout(r.Context(), 6*time.Second)
+			defer cancel()
+
+			hash := mux.Vars(r)["image_hash"]
+			res, err := engine.GetImage(hash)
+			if err != nil {
+				w.WriteHeader(http.StatusNotFound)
+				_, _ = w.Write([]byte(err.Error()))
+				return
+			}
+
+			_, _ = w.Write([]byte(res)) // a considerable amount of unchecked errors
+		},
+	).Methods(http.MethodGet)
+
+	go engine.Loop()
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func removeDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	var list []string
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
---
+++ resize_engine.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"hash/fnv"
+	"log"
+	"math/rand"
+	"runtime"
+	"strconv"
+	"sync"
+	"time"
+)
+
+const (
+	imageStatusSuccess    = "success"
+	imageStatusInProgress = "in progress"
+)
+
+type Image struct {
+	Result string `json:"result"`
+	URL    string `json:"url"`
+	Cached bool   `json:"cached"`
+}
+
+type ImageAndOutput struct {
+	url        string
+	outputChan chan Image
+}
+
+type ResizeEngine struct {
+	mu          sync.Mutex
+	imageInput  chan ImageAndOutput
+	quit        chan struct{}     // use this channel to trap a ctrl-c and break all for {} loops, gracefully terminating the workers
+	HashToImage map[string]*Image // maps hash to url and status
+}
+
+func NewResizeEngine() *ResizeEngine {
+	imageInput := make(chan ImageAndOutput, 100)
+	hashToImage := make(map[string]*Image)
+	quit := make(chan struct{}, 100)
+	return &ResizeEngine{
+		imageInput:  imageInput,
+		quit:        quit,
+		HashToImage: hashToImage,
+	}
+}
+
+func (l *ResizeEngine) Loop() {
+	var wg sync.WaitGroup
+	// waitgroups are the cheapest and most straightforward answer to worker pools
+	// there's also another answer to this which is more esoteric
+	// as always - the right tool for the right job
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		id := uuid.New()
+		go func() {
+			defer wg.Done()
+			l.worker(id.String())
+		}()
+	}
+}
+
+func (l *ResizeEngine) worker(id string) {
+	log.Printf("worker %s started\n", id)
+	for {
+		select {
+		case iao := <-l.imageInput:
+			log.Printf("worker %s got a job for %s\n", id, iao.url)
+
+			h := strconv.Itoa(int(hash(iao.url)))
+			// check if map has hash
+			l.mu.Lock()
+			if _, ok := l.HashToImage[h]; ok {
+				// if the hash is already in the map return the object on the channel (if the channel exists)
+				if iao.outputChan != nil {
+					iao.outputChan <- Image{
+						Result: l.HashToImage[h].Result,
+						URL:    l.HashToImage[h].URL,
+						Cached: l.HashToImage[h].Cached,
+					}
+				}
+				l.mu.Unlock()
+				continue
+			}
+
+			l.HashToImage[h] = &Image{
+				Result: imageStatusInProgress,
+				URL:    fmt.Sprintf("http://localhost:8080/v1/image/%s.jpeg", h),
+				Cached: false,
+			}
+			l.mu.Unlock()
+
+			time.Sleep(100 * time.Millisecond)
+			log.Println("fetching ", iao.url)
+			time.Sleep(time.Duration(rand.Intn(3)+5) * time.Second)
+			log.Println("fetched ", iao.url)
+			log.Println("resizing ", iao.url)
+			time.Sleep(time.Duration(rand.Intn(3)+5) * time.Second)
+			log.Println(iao.url, " resized")
+
+			l.mu.Lock()
+			l.HashToImage[h] = &Image{
+				Result: imageStatusSuccess,
+				URL:    fmt.Sprintf("http://localhost:8080/v1/image/%s.jpeg", h),
+				Cached: true,
+			}
+			l.mu.Unlock()
+			if iao.outputChan != nil {
+				l.mu.Lock()
+				iao.outputChan <- *l.HashToImage[h]
+				l.mu.Unlock()
+			}
+			//case <-l.quit:
+			//	log.Println(fmt.Sprintf("worker %s shutting down", id))
+			//	break
+		}
+	}
+}
+
+func (l *ResizeEngine) HashImageAsync(urls []string) []*Image {
+	var imageList []*Image
+	for _, url := range urls {
+		h := strconv.Itoa(int(hash(url)))
+
+		l.mu.Lock()
+		if _, ok := l.HashToImage[h]; ok { // hash already in map, get the object
+			imageList = append(imageList, &Image{
+				Result: l.HashToImage[h].Result,
+				URL:    l.HashToImage[h].URL,
+				Cached: l.HashToImage[h].Cached,
+			})
+			l.mu.Unlock() // defer not always the first choice
+			continue
+		}
+		imageList = append(imageList, &Image{ // move this in the worker and lock it while editing
+			Result: imageStatusInProgress,
+			URL:    fmt.Sprintf("http://localhost:8080/v1/image/%s.jpeg", h),
+			Cached: false,
+		})
+		l.mu.Unlock()
+
+		l.imageInput <- ImageAndOutput{
+			url:        url,
+			outputChan: nil,
+		}
+	}
+	return imageList
+}
+
+func (l *ResizeEngine) HashImageSync(urls []string) []*Image {
+	var (
+		imageList       []*Image
+		imagesToProcess = 0
+	)
+
+	oc := make(chan Image, len(urls))
+	for _, url := range urls {
+		h := strconv.Itoa(int(hash(url)))
+
+		l.mu.Lock()
+		if _, ok := l.HashToImage[h]; ok { // hash already in map, get the object
+			imageList = append(imageList, &Image{
+				Result: l.HashToImage[h].Result,
+				URL:    l.HashToImage[h].URL,
+				Cached: l.HashToImage[h].Cached,
+			})
+			l.mu.Unlock()
+			continue
+		}
+		l.mu.Unlock()
+
+		imagesToProcess++
+		l.imageInput <- ImageAndOutput{
+			url:        url,
+			outputChan: oc,
+		}
+	}
+
+	for len(imageList) != len(urls) {
+		image := <-oc
+		imageList = append(imageList, &image)
+	}
+
+	return imageList
+}
+
+func (l *ResizeEngine) GetImage(hash string) (string, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	// make use of early returns whenever appropriate
+	if _, ok := l.HashToImage[hash]; !ok { // if the hash isn't in storage return a 404
+		// we should return an error here that's cast in a meta err type with the appropriate response code
+		return "", fmt.Errorf("no image found for hash %s", hash)
+	}
+	// return a relevant message if the status != success
+	if l.HashToImage[hash].Result != imageStatusSuccess {
+		return l.HashToImage[hash].Result, nil
+	}
+	// return the "image"
+	return fmt.Sprintf("imagine this is the image for localhost/v1/image/%s.jpeg", hash), nil
+}
+
+func hash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
